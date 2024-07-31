bb75b30b625c1d583e4120458be3ed721907f313
Akanksha Dubey <akankshadubey2558@gmail.com>
2024-07-31 15:11:42 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -10,28 +10,28 @@ import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
 
-
 dotenv.config();
 
 connectDB();
-
 
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 cloudinary.config({
-	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-	api_key: process.env.CLOUDINARY_API_KEY,
-	api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Middlewares
-app.use(cors({
-	origin: 'http://localhost:5173', // Replace with your frontend's URL
-	credentials: true, // If you need to allow cookies or other credentials
-}));
-app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
-app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend's URL
+    credentials: true, // If you need to allow cookies or other credentials
+  })
+);
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
@@ -39,18 +39,21 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Serve frontend static files and handle React routing in production
-if (process.env.NODE_ENV === "development") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//http://localhost:5000=>backend,frontend
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-} else {
-	// In development, assume frontend is served separately and this only handles API
-	app.get("/", (req, res) => {
-		res.send("API running...");
-	});
-}
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+} 
+// else {
+//   app.get("/", (req, res) => {
+//     res.send("API running...");
+//   });
+// }
+
+server.listen(PORT, () =>
+  console.log(`Server started at http://localhost:${PORT}`)
+);
